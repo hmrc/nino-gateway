d102fdd1026f4b0eeb07526f2bab88649108145f
Saqib@Ee <saqib.saleem1@digital.hmrc.gov.uk>
2023-12-11 05:37:00 +0000

    CIR-1382: Play3 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,17 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
+  private val bootstrapPlayVersion = "8.1.0"
+
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "7.12.0"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.12.0" % "test, it"
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapPlayVersion % Test,
+  )
+
+  val itTest = Seq(
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapPlayVersion % "it",
   )
 }
